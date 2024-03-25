50e99e17bfdf389edd93ddefa29dc482ae28d35d
AmaanRS <amaanshaikh786420@gmail.com>
2024-03-25 17:14:41 +0530

    The data from the database is getting displayed on screen succesfully only sending the data is left and test the whole chat feature, later multimedia sending feature is also to be implemented

--- src/frontend/src/components/ChatArea.js
+++ src/frontend/src/components/ChatArea.js
@@ -1,29 +1,76 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
+import "../static/style.css";
 
 export default function ChatArea(props) {
+  //Rather than this directly use react
 
-    const addToConversation = ()=>{
-        let data = document.getElementById("outlined-multiline-flexible").value
-        
-        //Call a api to save the data as a conversation
+  // useEffect(()=>{
+  //   const addPreviousContentToChatArea = (()=>{
+  //     if(props.conversation === undefined) return
+  //     props.conversation.map((e)=>{
+  //       let side = ""
+  //       if(e.sender == "Self"){
+  //         //The message should be on right
+  //         side = "Right"
+  //       }else{
+  //         //The message should be on left
+  //         side = "Left"
+  //       }
+  //       const new_div = document.createElement("div")
+  //       new_div.innerText = e.message
+  //       new_div.setAttribute("class",side)
+  //       chatContentArea.appendChild(new_div)
+  //     })
+  //   })()
+  // },[])
 
-        //Show a the data in the #chatContent with div
-    }
+  const addToConversation = () => {
+    const chatContentArea = document.getElementById("chatContent");
 
-    console.log(props.conversation)
+    // The message typed in the chat box should be displayed on screen and saved to database using an api call
 
+  };
+
+  console.log(props.conversation);
 
   return (
     <>
       Chat area
       {props.email}
       <div id="chatArea" style={{ display: "flex", flexDirection: "column" }}>
-        <div style={{ height: "500px" }} id="chatContent"></div>
+        <div style={{ height: "500px" }} id="chatContent">
+          {props.conversation?.map((e, index) => {
+            return e.sender == "Self" ? (
+              <div
+                key={index}
+                style={{ display: "flex", justifyContent: "flex-end" }}
+              >
+                {e.message}
+              </div>
+            ) : (
+              <div
+                key={index}
+                style={{ display: "flex", justifyContent: "flex-start" }}
+              >
+                {e.message}
+              </div>
+            );
+          })}
+        </div>
         <div>
-          <TextField id="outlined-multiline-flexible" maxRows={4} style={{width:"800px"}} />
-          <Button onClick={addToConversation} style={{backgroundColor:"green", color:"white"}}>Send</Button>
+          <TextField
+            id="outlined-multiline-flexible"
+            maxRows={4}
+            style={{ width: "800px" }}
+          />
+          <Button
+            onClick={addToConversation}
+            style={{ backgroundColor: "green", color: "white" }}
+          >
+            Send
+          </Button>
         </div>
       </div>
     </>
